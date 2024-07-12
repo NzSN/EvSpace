5d1a9790f3327d7eb1130cc3ad391439ae6ed770
NzSN <nzsn0101@gmail.com>
2024-07-12 17:35:36 +0800

    Update typescript test

--- test.ts
+++ test.ts
@@ -1,7 +1,5 @@
 import Module from "./src/eval/spaces/wasm_space/wasm-space-cc/wasm-space.js";
-//import { createRequire } from "module";
-//const require = createRequire(import.meta.url);
-var native = require("./src/eval/spaces/node_native_space/hello");
+import * as native from "./src/eval/spaces/node_native_space/hello";
 
 test("Load module", () => {
     console.log(Module);
