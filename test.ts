77edfc440a26df37cfd0be8273a256cc2703c7cf
NzSN <nzsn0101@gmail.com>
2024-07-12 18:06:41 +0800

    move test to //test

--- test.ts
+++
@@ -1,7 +0,0 @@
-import Module from "./src/eval/spaces/wasm_space/wasm-space-cc/wasm-space.js";
-import * as native from "./src/eval/spaces/node_native_space/hello";
-
-test("Load module", () => {
-    console.log(Module);
-    console.log(native);
-})
---
+++ test/test.ts
@@ -0,0 +1,7 @@
+import Module from "../src/eval/spaces/wasm_space/wasm-space-cc/wasm-space.js";
+import * as native from "../src/eval/spaces/node_native_space/hello";
+
+test("Load module", () => {
+    console.log(Module);
+    console.log(native);
+})
