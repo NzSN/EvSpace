5db0c3e781a5e817e6dfaa529fef1a76641aa5e1
NzSN <nzsn0101@gmail.com>
2024-07-12 14:34:56 +0800

    Use swc as transpiler.

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,7 @@
 import Module from './eval/spaces/wasm_space/wasm-space-cc/wasm-space.js';
+import * as Native from './eval/spaces/node_native_space/hello';
 
 test("Simple", () => {
-    expect(true).toBeTruthy();
+    console.log(Module);
+    console.log(Native);
 })
